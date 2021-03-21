e8801a7454c57441c5e97e72d5262bba539000a4
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-03-21 18:58:07 -0300

    Shooting bullets

--- game1.js
+++ game1.js
@@ -30,7 +30,30 @@ class Player {
     }
 }
 
+class Bullets {
+    constructor(context, x, y, radius, color, velocity) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
 
+    draw() {
+        this.context.beginPath();
+        this.context.fillStyle = this.color;
+        this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        this.context.fill();
+
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
 
 //End .....................................................................
 
@@ -38,7 +61,30 @@ class Player {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 const p = new Player(c, x, y, 30, 'white');
+//array to group the bullets
+const bullets = [];
 
-//draw the player
-p.draw();
 
+function animate() {
+    //clean the frame
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    //draw the player
+    p.draw();
+    //draw and update the bullets
+    bullets.forEach((b) => {
+        b.update();
+    })
+
+    requestAnimationFrame(animate);
+}
+//mousemove
+addEventListener('click', (event) => {
+
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+
+    const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+
+    bullets.push(new Bullets(c, x, y, 5, 'red', velocity));
+})
+
+animate();
