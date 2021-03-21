8b912f49d91b2e78d1022190989fa4584247b5a8
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-03-21 20:27:59 -0300

    Creating enemy and detect collision on enemy by bullet

--- game1.js
+++ game1.js
@@ -3,6 +3,7 @@ const c = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+debug = false;
 //Objects .....................................................................
 class Player {
     constructor(context, x, y, radius, color) {
@@ -54,18 +55,77 @@ class Bullets {
         this.y += this.velocity.y;
     }
 }
+class Enemy {
+    constructor(context, x, y, radius, color, velocity) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
 
+    draw() {
+
+        this.context.beginPath();
+        this.context.fillStyle = this.color;
+        this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        this.context.fill();
+
+        if (debug) {
+            this.context.save();
+            this.context.beginPath();
+            this.context.fillText(this.x + ':\n' + this.y, this.x, this.y - this.radius);
+            this.context.restore();
+        }
+
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
 //End .....................................................................
 
 //Instating
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 const p = new Player(c, x, y, 30, 'white');
-//array to group the bullets
+//array to group the bullets and enemies
 const bullets = [];
+const enemies = [];
+const e = new Enemy(c, 100, 100, 30, 'green', { x: 1, y: 1 });
 
+//Enemies
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4
+        let x;
+        let y;
+        if (Math.random() < 0.5) {
+
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+        }
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const color = 'green'
+        const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+
+        enemies.push(new Enemy(c, x, y, radius, color, velocity));
+
+    }, 1000);
+
+}
 
 function animate() {
+    const frame = requestAnimationFrame(animate);
     //clean the frame
     c.clearRect(0, 0, canvas.width, canvas.height);
     //draw the player
@@ -75,8 +135,23 @@ function animate() {
         b.update();
     })
 
-    requestAnimationFrame(animate);
+    enemies.forEach((enemy, index) => {
+        enemy.update();
+        bullets.forEach((b, bulletIndex) => {
+            const dist = Math.hypot(b.x - enemy.x, b.y - enemy.y);
+
+            if (dist - enemy.radius - b.radius + 1 < 0.00001) {
+                console.info('remove from sreen:' + Number.parseFloat(dist - enemy.radius - b.radius));
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    bullets.splice(bulletIndex, 1);
+                }, 0);
+            }
+        });
+    });
+
 }
+
 //mousemove
 addEventListener('click', (event) => {
 
@@ -88,3 +163,4 @@ addEventListener('click', (event) => {
 })
 
 animate();
+spawnEnemies();
